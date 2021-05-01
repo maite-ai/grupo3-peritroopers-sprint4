d6cf8bb4e62ddcb8cb55713ff5cc9bfa3ab6dc9a
peirios <mariafernandarios89@gmail.com>
2021-05-01 18:24:47 -0300

    Todos - Finalizamos Sprint4

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 const path = require("path");
+const methodOverride = require("method-override");
 
 const publicPatch = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPatch));
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false}));
+app.use(methodOverride('_method'));
 
 //Le indicamos a express el requerimiento de rutas
 const homeRouter = require('./routes/homeRouter');
 const userRouter = require('./routes/userRouter');
 const productRouter = require('./routes/productRouter');
 
-app.use('/', userRouter);
-
+app.use('/users', userRouter);
 app.use('/', homeRouter);
-
-app.use('/', productRouter);
+app.use('/products', productRouter);
 
 app.listen(port || 3030, () => {
     console.log("El servidor está corriendo con éxito");
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller/productController');
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: path.resolve(__dirname, '../public/images'),
+    filename: (req, file, cb) => {
+        cb(null, 'img-' + Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({storage});
 
 router.get("/productCart", controller.productCart);
 
-router.get("/productDetail", controller.productDetail);
-
 router.get("/create", controller.create);
 
-router.get("/edit", controller.edit);
+router.get("/:id", controller.show);
+
+router.get('/search', controller.search);
+
+router.get("/:id/edit", controller.edit);
+
+router.post('/store', upload.single('image'), controller.store);
+
+router.put('/:id', upload.single('image'), controller.update);
+
+router.delete('/:id', controller.destroy);
 
 module.exports = router;
